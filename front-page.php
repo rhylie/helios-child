7d85fe8bbe02127303c06ccf681ecba110a2af51
Rhylie <rhylie@protonmail.ch>
2018-05-09 08:12:25 +0000

    progress-featured-home

--- front-page.php
+++ front-page.php
@@ -18,8 +18,12 @@ get_header();
 <!-- Banner -->
 	<section id="banner">
 		<header id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<h2><?php the_field('cta1home_head'); ?></h2>
-			<p><?php the_field('cta1home_text'); ?></p>
+			<?php 
+				$id=206; 
+				$post = get_post($id);
+				$content = apply_filters('the_content', $post->post_content); 
+				echo $content;  
+			?>
 		</header>
 	</section>
 
@@ -36,22 +40,10 @@ get_header();
 		<article id="main" class="container special">
 			<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
 			<header>
-				<h2><a href="#">Sed massa imperdiet magnis</a></h2>
-				<p>
-					Sociis aenean eu aenean mollis mollis facilisis primis ornare penatibus aenean. Cursus ac enim
-					pulvinar curabitur morbi convallis. Lectus malesuada sed fermentum dolore amet.
-				</p>
+				<h2><?php the_field('home_featured_heading')?></h2>
+				<p><?php the_field('home_featured_intro')?></p>
 			</header>
-			<p>
-				Commodo id natoque malesuada sollicitudin elit suscipit. Curae suspendisse mauris posuere accumsan massa
-				posuere lacus convallis tellus interdum. Amet nullam fringilla nibh nulla convallis ut venenatis purus
-				sit arcu sociis. Nunc fermentum adipiscing tempor cursus nascetur adipiscing adipiscing. Primis aliquam
-				mus lacinia lobortis phasellus suscipit. Fermentum lobortis non tristique ante proin sociis accumsan
-				lobortis. Auctor etiam porttitor phasellus tempus cubilia ultrices tempor sagittis. Nisl fermentum
-				consequat integer interdum integer purus sapien. Nibh eleifend nulla nascetur pharetra commodo mi augue
-				interdum tellus. Ornare cursus augue feugiat sodales velit lorem. Semper elementum ullamcorper lacinia
-				natoque aenean scelerisque.
-			</p>
+			<p><?php the_field('home_featured_description')?></p>
 			<footer>
 				<a href="#" class="button">Continue Reading</a>
 			</footer>
@@ -65,8 +57,8 @@ get_header();
 		<section id="features" class="container special">
 			<header>
 				<!-- TODO: Need to figure out a way to dynamically render cat name -->
-				<h2>Morbi ullamcorper et varius leo lacus</h2>
-				<p>Ipsum volutpat consectetur orci metus consequat imperdiet duis integer semper magna.</p>
+				<h2><?php echo get_cat_name(32);?></h2>
+				<p><?php echo category_description(32);?></p>
 			</header>
 
 			<div class="row">
@@ -75,8 +67,6 @@ get_header();
 		</section>
 
 	</div><!-- /.ends style1 -->
-
-
 <?php
 
 get_footer();
